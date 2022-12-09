53b9c25903ca1f06616ae3504675b8991b011e17
biechuyangwang <jiaolei1101049446@qq.com>
2022-12-09 11:16:01 +0800

    增加django示例

--- chatgpt/index.py
+++ chatgpt/index.py
@@ -9,6 +9,6 @@ from pychatgpt import Chat
 
 # Initializing the chat class will automatically log you in, check access_tokens
 chat = Chat(email="biechuyangwang@gmail.com", password="li123456")
-chat.cli_chat()
+# chat.cli_chat()
 answer = chat.ask("你好!")
 print(answer)
