61a244c9e772410824525007e0205644b6a1b0e4
biechuyangwang <jiaolei1101049446@qq.com>
2022-12-22 14:15:00 +0800

    ylgy前端

---
+++ ylgyclient/config.js
@@ -0,0 +1,8 @@
+const DEFAULT_SERVER_URL = "http://localhost:3500";
+
+if (!localStorage.getItem("server_url")) {
+  localStorage.setItem("server_url", DEFAULT_SERVER_URL);
+}
+
+window.SERVER_URL = localStorage.getItem("server_url");
+window.enableSettingsPage = true;
