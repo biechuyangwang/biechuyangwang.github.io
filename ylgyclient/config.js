3c9db148787cad9d8267d204e9051e0daaf5b6a6
biechuyangwang <jiaolei1101049446@qq.com>
2023-01-03 09:06:07 +0800

    修改ylgy客户端

--- ylgyclient/config.js
+++ ylgyclient/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_SERVER_URL = "http://localhost:3500";
+const DEFAULT_SERVER_URL = "http://120.24.4.30:4399";
 
 if (!localStorage.getItem("server_url")) {
   localStorage.setItem("server_url", DEFAULT_SERVER_URL);
