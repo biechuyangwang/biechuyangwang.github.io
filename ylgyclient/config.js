4a1f0ae89209ca865dc9579ede539a8015577549
biechuyangwang <jiaolei1101049446@qq.com>
2023-06-23 16:26:16 +0800

    更新前端

--- ylgyclient/config.js
+++ ylgyclient/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_SERVER_URL = "http://120.24.4.30:4399";
+const DEFAULT_SERVER_URL = "http://38.34.244.41:1357";
 
 if (!localStorage.getItem("server_url")) {
   localStorage.setItem("server_url", DEFAULT_SERVER_URL);
